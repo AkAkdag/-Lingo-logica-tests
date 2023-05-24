4fc244aa3790608f4577d37f6be322752839fe2f
AkAkdag <176484@student.aventus.nl>
2023-05-24 11:57:52 +0200

    update

--- Lingotestles1.py
+++ Lingotestles1.py
@@ -9,9 +9,11 @@ import tkinter as tk
 # Het woord 'main' is in dit geval een willekeurige naam die wordt gebruikt om te verwijzen naar het hoofdvenster
 class Raad: 
     def __init__(self, main): 
-        self.woord = "aarde"
+        self.woord = "perzik"
         self.main = main
-        self.main.title("Woord raden")  
+        self.main.title("Woord raden") 
+        self.beurten = 0 
+      
 
         self.speel_label = tk.Label(main, text="Raad een woord:")
         self.speel_label.pack()
@@ -21,11 +23,14 @@ class Raad:
 
         self.speel_button = tk.Button(main, text="Raad", command= self.spelen)
         self.speel_button.pack()
-
+        
+        self.letter_label = tk.Label(main, text="")
+        self.letter_label.pack() 
 
 #  Door  validate_input functie letters van een meegegeven woord wordt vergeleken met het te raden woord.
     def validate_input(self, input_woord):
-        if len (input_woord) != 5:
+        if len (input_woord) != 6:
+         self.letter_label.config(text = "Hint: Het woord bestaat uit 6 letters!")
          return False
         
         elif input_woord == self.woord:
@@ -34,25 +39,43 @@ class Raad:
         else:
             return False    
         
+  
 
 # Hier maken we een functie die spelen heet en geven we self als een parameter self verwijst naar het object (de instantie van de klasse (Raad)) waaraan de methode wordt opgeroepen. 
 # Als je wil je kan ook een andere naam geven in plaats van "self"
     def spelen(self):
         geraden = self.speel_entry.get()
         uitslag = self.validate_input(geraden)
+        self.beurten += 1
         if uitslag:
-            self.uitkomst_label.config(text="Je hebt het woord geraden :)")
+            self.uitkomst_label.config(text="Je hebt het woord geraden. :)") 
         else:
-            self.uitkomst_label.config(text="Jammer. Je hebt het woord niet geraden!")
+            if self.beurten >=5:
+                self.uitkomst_label.config(text="Je hebt het woord niet geraden.Het juiste woord was 'perzik'")
+                self.beurten = 0   
+            else:
+                self.uitkomst_label.config(text="Jammer. Je hebt het woord niet geraden!")   
+
+
+
+    def reset_spel(self):
+        self.beurten = 0
+        self.uitkomst_label.config(text="")
+        self.speel_entry.delete(0, tk.END)
 
 # Hier maken we een functie die uitvoer heet en geven we self als een parameter self verwijst naar het object (de instantie van de klasse (Raad)) waaraan de methode wordt opgeroepen. 
 # Hier verbinden we attribuut (uitkomt) van vorige functie met behulp van self parameter 
     def uitvoer(self):
         self.uitkomst_label = tk.Label(self.main, text="")
         self.uitkomst_label.pack()
+        
+        self.herstarten_label= tk.Button(self.main, text="Herstarten", command= self.reset_spel)
+        self.herstarten_label.pack()
+        
+
         self.main.mainloop()              
 
-
+        
 # Hier noemen we de klasse en functies zodat het programma wordt gestart
 window = tk.Tk()
 woord = Raad(window)
