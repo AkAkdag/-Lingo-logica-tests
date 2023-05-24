da5326e2cae7d8dd99342a57c75b0b781ad13ca9
AkAkdag <176484@student.aventus.nl>
2023-05-24 09:25:06 +0200

    update

--- Lingo logica.py
+++ Lingo logica.py
@@ -1,6 +1,26 @@
-class Raad:
-    def __init__(self, woord_aarde): 
-        self.aarde = woord_aarde
 
- def validate_input():
-    
+class Raad:
+    def __init__(self): 
+        self.woord = "aarde"
+
+    def validate_input(self, input_woord):
+        if len (input_woord) != 5:
+         return False
+        
+        elif input_woord == self.woord:
+            return True
+        
+        else:
+            return False    
+
+
+woord = Raad()
+
+geraden = input ("Doe een gok ")
+uitslag = woord.validate_input(geraden)
+if uitslag:
+    print("Je hebt het woord geraden :)")
+else:
+    print("Jammer. Je hebt het woord niet geraden!")
+
+
