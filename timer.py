3060aa34dba9acfc08d66d9e0d0c310831e474dc
AkAkdag <176484@student.aventus.nl>
2023-06-14 12:02:24 +0200

    update

---
+++ timer.py
@@ -0,0 +1,17 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.end_time = time.time()
+
+    def elapsed_time(self):
+        if self.start_time is None or self.end_time is None:
+            return None
+        return self.end_time - self.start_time
